d5cc96c8ec60ab32de346bdaa1d322c0cc211df9
jwywoo <jwywoo26@gmail.com>
2023-06-21 20:44:47 +0900

    Brute Force in JAVA: BOJ 15649 done

--- InJava/CodingTestBruteForce/Main.java
+++ InJava/CodingTestBruteForce/Main.java
@@ -44,3 +44,50 @@ import java.util.Scanner;
 //}
 
 
+// 15694
+public class Main {
+    static int N;
+    static int M;
+    static int[] selected;
+    static int[] used;
+    static StringBuilder sb = new StringBuilder();
+
+    static void input() {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        selected = new int[M+1];
+        used = new int[N+1];
+    }
+
+    static void recursiveSol(int k) {
+        // base case
+        if (k == M + 1) {
+            for (int i = 1; i <= M; i++) {
+                sb.append(selected[i]).append(' ');
+            }
+            sb.append('\n');
+        }
+        // recursion
+        else {
+            for (int cand = 1; cand <= N; cand++) {
+                if (used[cand] == 1) continue;
+                selected[k] = cand;
+                used[cand] = 1;
+
+                recursiveSol(k + 1);
+
+                selected[k] = 0;
+                used[cand] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        recursiveSol(1);
+
+        System.out.println(sb.toString());
+    }
+}
