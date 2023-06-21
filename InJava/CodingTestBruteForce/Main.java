afe2cc646fd79ff48d767faf40d813651dc1bf6c
jwywoo <jwywoo26@gmail.com>
2023-06-21 21:25:00 +0900

    Brute Force in JAVA: BOJ 15652 Start

--- InJava/CodingTestBruteForce/Main.java
+++ InJava/CodingTestBruteForce/Main.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 // 15651
 //public class Main {
 //    static int N;
@@ -45,20 +44,70 @@ import java.util.Scanner;
 
 
 // 15694
+//public class Main {
+//    static int N;
+//    static int M;
+//    static int[] selected;
+//    static int[] used;
+//    static StringBuilder sb = new StringBuilder();
+//
+//    static void input() {
+//        Scanner sc = new Scanner(System.in);
+//        N = sc.nextInt();
+//        M = sc.nextInt();
+//
+//        selected = new int[M+1];
+//        used = new int[N+1];
+//    }
+//
+//    static void recursiveSol(int k) {
+//        // base case
+//        if (k == M + 1) {
+//            for (int i = 1; i <= M; i++) {
+//                sb.append(selected[i]).append(' ');
+//            }
+//            sb.append('\n');
+//         }
+//        // recursion
+//        else {
+//            for (int cand = 1; cand <= N; cand++) {
+//                if (used[cand] == 1) continue;
+//                selected[k] = cand;
+//                used[cand] = 1;
+//
+//                recursiveSol(k + 1);
+//
+//                selected[k] = 0;
+//                used[cand] = 0;
+//            }
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        input();
+//        recursiveSol(1);
+//
+//        System.out.println(sb.toString());
+//    }
+//}
+
+// 15652
+import java.util.Scanner;
+
 public class Main {
     static int N;
     static int M;
     static int[] selected;
-    static int[] used;
+
     static StringBuilder sb = new StringBuilder();
 
     static void input() {
         Scanner sc = new Scanner(System.in);
+
         N = sc.nextInt();
         M = sc.nextInt();
 
-        selected = new int[M+1];
-        used = new int[N+1];
+        selected = new int[M + 1];
     }
 
     static void recursiveSol(int k) {
@@ -71,15 +120,10 @@ public class Main {
         }
         // recursion
         else {
-            for (int cand = 1; cand <= N; cand++) {
-                if (used[cand] == 1) continue;
-                selected[k] = cand;
-                used[cand] = 1;
-
-                recursiveSol(k + 1);
-
+            for (int i = 1; i <= N; i++) {
+                selected[k] = i;
+                recursiveSol(k+1);
                 selected[k] = 0;
-                used[cand] = 0;
             }
         }
     }
