a4ba789707d89291ad6039589dccc9c5cdbad5f7
jwywoo <jwywoo26@gmail.com>
2023-06-24 18:37:41 +0900

    Two Pointers: Partial Sum done

---
+++ InJava/CodingTestTwoPointers/src/TwoLiquids.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TwoLiquids {
+    static Scanner sc = new Scanner(System.in);
+
+    static int N;
+    static int seq[];
+    static void input() {
+        N = sc.nextInt();
+        seq = new int[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            seq[i] = sc.nextInt();
+        }
+    }
+    static void pro() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
