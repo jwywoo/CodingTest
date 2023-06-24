f187349b82819d6d39dce372d3967e6706459181
jwywoo <jwywoo26@gmail.com>
2023-06-24 19:49:40 +0900

    Two pointers practice: 13144 Done

--- InJava/CodingTestTwoPointersPrac/src/Main.java
+++ InJava/CodingTestTwoPointersPrac/src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static int N;
+    static int[] A;
+    static int[] count;
+
+
+    static void input() {
+        N = sc.nextInt();
+        A = new int[N+1];
+        count = new int[100000+1];
+
+        for (int i = 1; i <= N; i++) {
+            A[i] = sc.nextInt();
+        }
+    }
+
+    static void pro() {
+        long ans = 0;
+
+        for (int L = 1, R=0; L<=N; L++) {
+            while (R + 1 <= N && count[A[R+1]] == 0) {
+                R++;
+                count[A[R]]++;
+            }
+            ans += R-L+1;
+            count[A[L]] --;
+        }
+        System.out.println(ans);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        input();
+        pro();
     }
 }
