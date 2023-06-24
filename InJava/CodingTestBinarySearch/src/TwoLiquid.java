f709a6e568f1d87af836291ed135ea87a2dd255d
jwywoo <jwywoo26@gmail.com>
2023-06-24 15:54:49 +0900

    Binary Search: Two liquids done

--- InJava/CodingTestBinarySearch/src/TwoLiquid.java
+++ InJava/CodingTestBinarySearch/src/TwoLiquid.java
@@ -1,36 +1,72 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TwoLiquid {
-    static int N, M;
+    static Scanner sc = new Scanner(System.in);
+    static StringBuilder sb = new StringBuilder();
 
-    static int[] A,B;
+    static int N;
+    static int[] A;
+
     static void input() {
-
+        N = sc.nextInt();
+        A = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            A[i] = sc.nextInt();
+        }
     }
 
+    // Memorize this shit
     static int lowerBound(int[] A, int L, int R, int X) {
-        int res = L - 1;
+        // A[L...R] 에서 X 이상의 수 중 제일 왼쪽 인덱스를 return 하는 함수
+        // 그런 게 없다면 R + 1 을 return 한다
+
+        int res = R + 1;  // 만약 A[L...R] 중
         while (L <= R) {
-            int mid = (L+R)/2;
-            if (A[mid] < X) {
-                L = mid + 1;
-            }
-            else  {
+            int mid = (L + R) / 2;
+            if (A[mid] >= X) {
+                res = mid;
                 R = mid - 1;
+            } else {
+                L = mid + 1;
             }
         }
         return res;
     }
-    static void pro() {
-        // Sort Target Group
-        Arrays.sort(B);
-        // Loop
-        int ans = 0;
-        for (int i = 1; i <= N; i++) {
-            ans += lowerBound(B, 1, N+1, A[i]);
-        }
-    }
-    public static void main(String[] args) {
 
+    static void pro() {
+        // A 에 대해 이분 탐색을 할 예정이니까, 정렬을 미리 해주자.
+        Arrays.sort(A, 1, N + 1);
+
+        int best_sum = Integer.MAX_VALUE;
+        int v1 = 0, v2 = 0;
+        for (int left = 1; left <= N - 1; left++) {
+            // A[left] 용액을 쓸 것이다. 고로 -A[left] 와 가장 가까운 용액을 자신의 오른쪽 구간에서 찾자.
+            int candidate = lowerBound(A, left + 1, N, -A[left]);
+
+            // A[candidate - 1] 와 A[candidate] 중에 A[left] 와 섞었을 때의 정보를 정답에 갱신시킨다.
+
+            // 1. A[left] + A[candidate - 1]
+            if (left < candidate - 1 && Math.abs(A[left] + A[candidate - 1]) < best_sum) {
+                best_sum = Math.abs(A[left] + A[candidate - 1]);
+                v1 = A[left];
+                v2 = A[candidate - 1];
+            }
+
+            // 2. A[left] + A[candidate]
+            if (candidate <= N && Math.abs(A[left] + A[candidate]) < best_sum) {
+                best_sum = Math.abs(A[left] + A[candidate]);
+                v1 = A[left];
+                v2 = A[candidate];
+            }
+
+        }
+        sb.append(v1).append(' ').append(v2);
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
     }
 }
