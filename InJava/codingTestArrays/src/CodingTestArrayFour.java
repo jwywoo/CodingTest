181a142eeb439474b25dfde85f177857bca2c66c
jwywoo <jwywoo26@gmail.com>
2023-07-17 17:18:47 +0900

    Coding Test Arrays: 3273 Pause

---
+++ InJava/codingTestArrays/src/CodingTestArrayFour.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// 3273
+public class CodingTestArrayFour {
+    static Scanner sc = new Scanner(System.in);
+    // 9 Length (1 ~ 100000)
+    // 5 12 7 10 9 1 2 3 11 Array
+    // 13 X
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> givenArray = new ArrayList<>();
+        String[] tempArray = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            givenArray.add(Integer.parseInt(tempArray[i]));
+        }
+        int X = Integer.parseInt(br.readLine());
+        int result = 0;
+//        for (int i = 0; i < N; i++) {
+//            Integer temp = X - givenArray.get(i);
+//            result += Collections.frequency(givenArray.subList(i, givenArray.size()), temp );
+//        }
+
+        System.out.println(result);
+    }
+}
