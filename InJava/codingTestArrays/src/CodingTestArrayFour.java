2e98fd83a01e170296ad7286f0d44027a62c2713
jwywoo <jwywoo26@gmail.com>
2023-07-19 14:50:57 +0900

    Coding Test Arrays: 3273 Done indexing by making another list

--- InJava/codingTestArrays/src/CodingTestArrayFour.java
+++ InJava/codingTestArrays/src/CodingTestArrayFour.java
@@ -13,18 +13,30 @@ public class CodingTestArrayFour {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        int N = sc.nextInt();
         int N = Integer.parseInt(br.readLine());
-        List<Integer> givenArray = new ArrayList<>();
+        int[] givenArray = new int[N];
+        boolean[] checking = new boolean[1000001];
         String[] tempArray = br.readLine().split(" ");
         for (int i = 0; i < N; i++) {
-            givenArray.add(Integer.parseInt(tempArray[i]));
+            givenArray[i] = Integer.parseInt(tempArray[i]);
         }
         int X = Integer.parseInt(br.readLine());
         int result = 0;
+        // Boolean list
+        for (int i = 0; i < N; i++) {
+            checking[givenArray[i]] = true;
+        }
+
+        for (int i = 0; i < N; i++) {
+            int valueNeeded = X - givenArray[i];
+            if (valueNeeded > 0 && valueNeeded < 1000000)
+                if (checking[valueNeeded]) result++;
+        }
+
 //        for (int i = 0; i < N; i++) {
 //            Integer temp = X - givenArray.get(i);
 //            result += Collections.frequency(givenArray.subList(i, givenArray.size()), temp );
 //        }
 
-        System.out.println(result);
+        System.out.println(result/2);
     }
 }
