50977b24f0ccb773cb4f414fbc1c8337bbc66ad7
jwywoo <jwywoo26@gmail.com>
2023-07-27 21:59:19 +0900

    Coding Test Partial Sum: 11659 done, preparing two arrays one for input one for partial sum arr[N+1]

--- InJava/CodingTestPartialSum/src/main/java/org/example/CodingTestPartialSumOne.java
+++ InJava/CodingTestPartialSum/src/main/java/org/example/CodingTestPartialSumOne.java
@@ -1,4 +1,30 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class CodingTestPartialSumOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+        int[] givenInt = new int[N + 1];
+        int[] givenSum = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            givenInt[i] = sc.nextInt();
+            if (i == 1) {
+                givenSum[i] = givenInt[i];
+            } else {
+                givenSum[i] = givenSum[i - 1] ^ givenInt[i];
+            }
+        }
+
+        int result = 0;
+        for (int i = 1; i <= X; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            result ^= givenSum[end] ^ givenSum[start-1];
+        }
+
+        System.out.println(result);
+    }
 }
---
+++ InJava/CodingTestPartialSum/src/main/java/org/example/CodingTestPartialSumTwo.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.io.*;
+
+// 11660
+// Buffered reader and writer
+public class CodingTestPartialSumTwo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
